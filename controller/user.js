ecefc398a5a178d0492089f307dcf27abb970530
shivam <shiv10212005@gmail.com>
2025-10-28 22:45:24 +0530

    first commit

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,17 @@
+const User = require('../models/user');
+
 exports.register = async (req,res) => {
     try{
         console.log(req.body);
         let {email,password, f_name} = req.body;
+        let isUserExist = await User.findOne({email});
+        if(isUserExist){
+            res.status(400).json({error: "Already have an account with this email .Please try with other email."});
+        }
+        const newUser = new User({email,password,f_name});
+        await newUser.save();
+
+        return res.status(201).json({message: 'User registered successfully', success: "yes", date: newUser});
 
     }catch(err){
         console.log(err);
